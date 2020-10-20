03fe80e4b18361f11c07b5e6dafcc58d75078a51
Susana Guzman <zuxi.w13@gmail.com>
2020-10-20 12:54:26 +0200

    test: first test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,169 +1,176 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-2"
+  access_key = "AKIAWB5UZPEQKC6SHRMS"
+  secret_key = "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"
+}
+
+#Create first service
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-2c"
+}
+
+resource "aws_ecr_repository" "southpark_repo" {
+  name = "southpark_repo" 
+}
+
+#Create cluster
+resource "aws_ecs_cluster" "ecs_southpark" {
+  name = "ecs_southpark" 
+}
+
+#Create task
+resource "aws_ecs_task_definition" "southpark_task" {
+  family                   = "southpark_task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "southpark_task",
+      "image": "${aws_ecr_repository.southpark_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-2"
-  access_key = AWS_ACCESS_KEY_ID
-  secret_key = AWS_SECRET_ACCESS_KEY
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-07efac79022b86107"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "ubuntu"
-  }
+#Create load balancer
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
 
-#Create VPC
-resource "aws_vpc" "my_first_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "production"
-  }
-}
-
-resource "aws_vpc" "my_second_vpc" {
-  cidr_block = "172.18.0.0/16"
-
-  tags = {
-    Name = "dev"
-  }
-}
-
-resource "aws_vpc" "prod-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "production"
-  }
-}
-
-#Create Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.prod-vpc.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-# Create Route table
-resource "aws_route_table" "prod-route-table" {
-  vpc_id = aws_vpc.prod-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0" #all traffic will be sent to the gateway
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Prod"
-  }
-}
-
-#Create Subnet
-resource "aws_subnet" "subnet-new-1" {
-  vpc_id = aws_vpc.prod-vpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "prod-subnet"
-  } 
-}
-
-#Associate subnet with Route table 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-new-1.id
-  route_table_id = aws_route_table.prod-route-table.id
-}
-
-#Create securty group to allow ports 443, 80 and 22
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow Web inbound traffic"
-  vpc_id = aws_vpc.prod-vpc.id
-
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
+    from_port   = 80 # Allowing traffic in from port 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_web"
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
   }
 }
 
-#Create network interface with an IP from subnet (this creates a private IP address )
-resource "aws_network_interface" "web-server-ni   c" {
-  subnet_id       = aws_subnet.subnet-new-1.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_web.id]
-}
-
-#Set elastic IP addres (public IP adress )
-resource "aws_eip"   "one"  {
-  vpc       = true
-  network_interface = aws_network_interface.web-server-nic.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.gw] 
-}
-
-#Create Ubunbut server and install apace
-resource "aws_instance" "web-server-instance" {
-  ami = "ami-07efac79022b86107"
-  instance_type = "t2.micro"
-  key_name = "jina-aws-key"
-
-  network_interface {
-    device_index = 0
-    network_interface_id = aws_network_interface.web-server-nic.id
-  }
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install apache2 -y
-              sudo systemctl start apache2
-              sudo bash -c 'echo first web server   > /var/www/html/index.html'
-              EOF
-  tags = {
-    "Name" = "web-server"
+#Redirect traffic
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
   }
 }
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
+
+
+resource "aws_ecs_service" "southpark_service" {
+  name            = "southpark_service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.ecs_southpark.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.southpark_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  #Link ECS to load balancer  
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.southpark_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+
+
