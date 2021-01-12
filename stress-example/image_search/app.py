e181e3425f7a2ee972754e1fad70e1994dadcd35
Susana Guzman <zuxi.w13@gmail.com>
2021-01-12 16:22:44 +0100

    fix: add channel_axis

--- stress-example/image_search/app.py
+++ stress-example/image_search/app.py
@@ -7,22 +7,21 @@ import sys
 from jina import Document
 from jina.flow import Flow
 
-num_docs = 500
+num_docs = 50
 
 
 def create_random_img_array(img_height, img_width):
     import numpy as np
     return np.random.randint(0, 256, (img_height, img_width, 3))
 
+
 def random_docs():
     for idx in range(0, num_docs):
         with Document() as doc:
             doc.id = idx
-            doc.blob = create_random_img_array(3, 3)
+            doc.blob = create_random_img_array(25, 25)
             doc.mime_type = 'image/png'
-            #print('#################################################shape ', doc.blob.shape)
         yield doc
-
 
 
 def config():
@@ -31,23 +30,21 @@ def config():
 
     os.environ.setdefault('JINA_PARALLEL', str(parallel))
     os.environ.setdefault('JINA_SHARDS', str(shards))
-    os.environ.setdefault('JINA_WORKSPACE', '../workspace')
+    os.environ.setdefault('JINA_WORKSPACE', './workspace')
     os.environ.setdefault('JINA_PORT', str(45678))
 
 
 # for index
 def index():
-    f = Flow.load_config('flows/index.yml')
-    with f:
-        f.index(input_fn=random_docs())
+    with Flow.load_config('flows/index.yml') as index_flow:
+        index_flow.index(input_fn=random_docs())
 
 
 # for search; annoy, faiss, scann with refIndexer
-def search():
-    f = Flow.load_config('flows/query.yml')
+def query():
+    with Flow.load_config('flows/query.yml') as search_flow:
+        search_flow.block()
 
-    with f:
-        f.block()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -63,8 +60,8 @@ if __name__ == '__main__':
                     \n |                                   🤖🤖🤖                                        | \
                     \n +---------------------------------------------------------------------------------+')
         index()
-    elif sys.argv[1] == 'search':
+    elif sys.argv[1] == 'query':
         config()
-        search()
+        query()
     else:
         raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
