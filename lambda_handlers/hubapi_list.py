bef63229ae745a3f8e0455601af7d28050e6f6f4
rutujasurve94 <rutuja.r.surve@gmail.com>
2021-01-14 18:11:50 +0530

    fix: test hub list

--- lambda_handlers/hubapi_list.py
+++ lambda_handlers/hubapi_list.py
@@ -71,9 +71,6 @@ class MongoDBHandler:
 
     def find_many(self, query: Dict[str, Union[Dict, List]], limit: int = 0) -> None:
         try:
-            print('*** inside find many *** ')
-            print('*** collection count inside find many is *** '+ str(self.collection.count()))
-            print('*** printing collection contents *** '+ str(self.collection.find()))
             return self.collection.find(filter=query, limit=limit)
         except pymongo.errors.PyMongoError as exp:
             self.logger.error(f'got an error while finding a document in the db {exp}')
@@ -114,7 +111,6 @@ def _query_builder(params: Dict):
         return {}, 0
 
     sub_query = []
-
     if 'kind' in params:
         kind_query = {'manifest_info.kind': params['kind']}
         sub_query.append(kind_query)
@@ -159,11 +155,11 @@ def str_to_ascii_to_base64_to_str(text):
 
 
 def read_environment():
-    hostname = (os.environ.get('JINA_DB_HOSTNAME'))
-    username = (os.environ.get('JINA_DB_USERNAME'))
-    password = (os.environ.get('JINA_DB_PASSWORD'))
-    database_name = (os.environ.get('JINA_DB_NAME'))
-    collection_name = (os.environ.get('JINA_DB_COLLECTION'))
+    hostname = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_HOSTNAME'))
+    username = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_USERNAME'))
+    password = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_PASSWORD'))
+    database_name = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_NAME'))
+    collection_name = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_COLLECTION'))
     return hostname, username, password, database_name, collection_name
 
 
@@ -172,21 +168,16 @@ def lambda_handler(event, context):
     """
     logger = get_logger(context='hub_list')
 
-
     if not is_db_envs_set():
         logger.warning('MongoDB environment vars are not set! book-keeping skipped.')
         return _return_json_builder(body='Invalid Lambda environment',
                                     status=500)
 
     _executor_query = _query_builder(params=event.get('queryStringParameters', {}))
-    
-    print('*** executor query iss *** '+ str(_executor_query))
+
     hostname, username, password, database_name, collection_name = read_environment()
     with MongoDBHandler(hostname=hostname, username=username, password=password,
                         database_name=database_name, collection_name=collection_name) as db:
-        print('***** inspecting collection *** ')
-        print(str(type(db.collection)))
-        #print(str(db.collection.find()))
         existing_docs = db.find_many(*_executor_query)
         if existing_docs:
             all_manifests = [doc['manifest_info'] for doc in existing_docs]
--- lambda_handlers/tests/unit/test_hubapi_list.py
+++ lambda_handlers/tests/unit/test_hubapi_list.py
@@ -19,10 +19,12 @@ from pymongo import MongoClient
 @pytest.fixture
 def mock_pymongo_mongoclient():
     client = mongomock.MongoClient()
-    sample_collection_objects = [{'_id':1, 'name': 'first', 'manifest_info':{'kind': 'encoder', 'type': 'pod'}}, {'_id':2, 'name': 'second', 'manifest_info':{'kind': 'encoder', 'type': 'pod'}}]
-    # for obj in sample_collection_objects:
-    #     obj['_id'] = client.db.collection.insert_one(obj).inserted_id
-    # print('** fix collection count is *** '+ str(client.db.collection.count()))
+    sample_collection_objects = []
+    with open('mongo_list_objs.json', "r") as read_file:
+        mongo_objs = json.load(read_file)
+        sample_collection_objects = mongo_objs["collection"]
+    for obj in sample_collection_objects:
+        obj['_id'] = client.db.collection.insert_one(obj).inserted_id
     return client
 
 def test_list(mocker, monkeypatch, mock_pymongo_mongoclient):
@@ -36,71 +38,21 @@ def test_list(mocker, monkeypatch, mock_pymongo_mongoclient):
     event = None
     with open('event.json', "r") as read_file:
         event = json.load(read_file)
-
-    sample_collection_objects = [{'_id':111, 'name': 'first', 'manifest_info':{'kind': 'encoder', 'type': 'pod'}}, {'_id':222, 'name': 'second', 'manifest_info':{'kind': 'encoder', 'type': 'pod'}}]
-    mock_collection = mock_pymongo_mongoclient.db.collection #mongomock.MongoClient().db.collection 
-    mock_collection.insert_many(sample_collection_objects)
-    # for obj in sample_collection_objects:
-    #     #mock_collection.insert_one(obj)
-    #     obj['_id'] = mock_collection.insert_one(obj).inserted_id
-    assert mock_collection.count() == 2
-    print('** mox count is *** '+ str(mock_collection.count()))
-    print('*****mox  collections has objs **** '+str(list(mock_collection.find())))
-    #mock_client.return_value.db.collection = mock_collection #mongomock.Collection
-    #mock_pymongo_mongoclient.return_value.db.collection = mock_collection
-
-    #mock_client.return_value.collection = mock_collection #mongomock.Collection
-    #mock_pymongo_mongoclient.return_value.collection = mock_collection
-
-
     use_query = _query_builder(params=event.get('queryStringParameters', {}))
 
+    assert use_query[0] == {'$and': [{'manifest_info.kind': 'encoder'}, {'manifest_info.type': 'pod'}]}
+
+    # assert that mock collection has 3 objects, two with kind encoder, one with type indexer, type pod.
+    mock_collection = mock_pymongo_mongoclient.db.collection
+    assert mock_collection.count() == 3
+
+  
     mock_client = mocker.patch.object(MongoDBHandler, 'client', autospec=True)
     mock_client.return_value = mock_pymongo_mongoclient
-    mock_client.return_value.find_many = MongoDBHandler.find_many(mock_client, query=use_query)
 
     mock_connect = mocker.patch.object(MongoDBHandler, 'connect', autospec=True)
-    mock_connect.return_value = mock_pymongo_mongoclient
     mock_connect.return_value.close = None
   
-    objs = []
-    with open('mongo_list_objs.json', "r") as read_file:
-        mongo_objs = json.load(read_file)
-        objs = mongo_objs["collection"]
-    # mock_pymongo_mongoclient.collection = objs
-    # mock_client.return_value.collection = objs #mongomock.Collection
-
-
-    # mock_my_collection = mocker.patch.object(MongoDBHandler, 'collection', autospec=True)
-    # mock_my_collection.return_value = sample_collection_objects
-    # print(str(objs))
-    # print(mongo_objs)
-
-    with mock.patch('lambda_handlers.hubapi_list.MongoDBHandler.collection', new_callable=mock.PropertyMock) as mock_handler_collection:
-        mock_handler_collection.return_value = mock_collection
-
-        # mock_my_collection = mocker.patch.object(MongoDBHandler, 'collection', autospec=True)
-        # mock_my_collection.return_value =  mock_collection #sample_collection_objects
-        # monkeypatch.setattr(MongoDBHandler, "collection", mock_collection) #sample_collection_objects
-
-        type(mock_pymongo_mongoclient).collection = mock.PropertyMock(return_value=mock_collection)
-        type(mock_client).collection = mock.PropertyMock(return_value=mock_collection)
-        type(mock_pymongo_mongoclient).collection.find = mock.PropertyMock(return_value=sample_collection_objects)
-        type(mock_client).collection.find = mock.PropertyMock(return_value=sample_collection_objects)
-
-        result = lambda_handler(event, None)
-        print('*** result items are **** ')
-        print('*******    *******')
-        print(str(result.items()))
-        mongomock.database.Collection
-
-
-
-
-
-
-# lots to learn from this
-# reason could be : I should mock the MongoDBHandler class oject itself - set it's client property to mongomock
-# not the entire object itself
-# then set its database proprty to mongomock db, similarly collection, find_many - this is the right way to go
-# so find_many would invoke mongomock's find- this seems to be crux
+    # assert that 2 objects with kind encoder are returned, type pod
+    assert mock_collection.count(use_query[0]) == 2
+    result = lambda_handler(event, None)
