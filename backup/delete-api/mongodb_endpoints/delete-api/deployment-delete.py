86a2eb64ae8dce72035dcdd88ad355d176e1cdde
Rutuja Surve <rutuja.r.surve@gmail.com>
2021-01-14 15:14:27 +0530

    feat: add delete api for mongodb (#61)

---
+++ backup/delete-api/mongodb_endpoints/delete-api/deployment-delete.py
@@ -0,0 +1,143 @@
+import os
+import sys
+import base64
+
+import click
+
+sys.path.append('..')
+
+from aws.helper import file_exists, read_file_content, is_aws_cred_set, is_db_envs_set
+from aws.logger import get_logger
+from aws.excepts import StackCreationFailed, StackUpdateFailed
+from aws.services.s3 import S3
+from aws.services.cloudformation import CFNStack
+
+
+def str_to_ascii_to_base64_to_str(text):
+    return base64.b64encode(text.encode('ascii')).decode('ascii')
+
+
+def read_environment():
+    hostname = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_HOSTNAME'))
+    username = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_USERNAME'))
+    password = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_PASSWORD'))
+    database_name = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_NAME'))
+    collection_name = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_COLLECTION'))
+    docker_username = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DOCKER_USERNAME'))
+    docker_password = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DOCKER_PASSWORD'))
+    return hostname, username, password, database_name, collection_name, docker_username, docker_password
+
+
+@click.command()
+@click.option('--list-deployment-zip',
+              help='Deployment package zip to be used with HubList Lambda function')
+@click.option('--push-deployment-zip',
+              help='Deployment package zip to be used with HubPush Lambda function')
+@click.option('--authorize-deployment-zip',
+              help='Deployment package zip to be used with HubPush Lambda function')
+@click.option('--delete-deployment-zip',
+              help='Deployment package zip to be used with HubDelete Lambda function')
+@click.option('--docker-cred-deployment-zip',
+              help='Deployment package zip to be used with DockerCredFetcher Lambda function')
+@click.option('--key-id',
+              help='Enter folder to be created in S3 (will take PR# in GH Action)')
+@click.option('--stack-name',
+              default='jinahub-api-stack2',
+              help='Name of the CFN Stack (Defaults to jinahub-api-stack2)')
+@click.option('--template',
+              default='cloud_ymls/apigateway.yml',
+              help='CFN Template to be used for deployment (Default - cloud_ymls/apigateway.yml)')
+@click.option('--deployment-stage',
+              default='dev',
+              help='Deployment stage for API Gateway (Default - dev)')
+def trigger(list_deployment_zip, push_deployment_zip, authorize_deployment_zip, delete_deployment_zip, docker_cred_deployment_zip, key_id,
+            stack_name, template, deployment_stage):
+    logger = get_logger(__name__)
+
+    if not is_aws_cred_set():
+        logger.error('AWS Creds are not set! Exiting!')
+        sys.exit(1)
+
+    if not is_db_envs_set():
+        logger.error('MongoDB environment vars needed for lambda functions are not set! Exiting!')
+        sys.exit(1)
+
+    if not file_exists(filepath=list_deployment_zip) and not file_exists(filepath=push_deployment_zip):
+        logger.error('Both zip file locations are invalid. Exiting!')
+        sys.exit(1)
+
+    if not file_exists(filepath=template):
+        logger.error('Invalid cfn yaml. Exiting!')
+        sys.exit(1)
+
+    S3_DEFAULT_BUCKET = 'lambda-handlers-jina'
+    s3 = S3(bucket=S3_DEFAULT_BUCKET)
+
+    if list_deployment_zip is not None:
+        zip_filename = os.path.basename(list_deployment_zip)
+        s3_list_key = f'hubapi_list/{key_id}/{zip_filename}'
+        s3.put(filepath=list_deployment_zip,
+               key=s3_list_key)
+
+    if push_deployment_zip is not None:
+        zip_filename = os.path.basename(push_deployment_zip)
+        s3_push_key = f'hubapi_push/{key_id}/{zip_filename}'
+        s3.put(filepath=push_deployment_zip,
+               key=s3_push_key)
+
+    if authorize_deployment_zip is not None:
+        zip_filename = os.path.basename(authorize_deployment_zip)
+        s3_authorize_key = f'hubapi_authorize/{key_id}/{zip_filename}'
+        s3.put(filepath=authorize_deployment_zip,
+               key=s3_authorize_key)
+
+    if delete_deployment_zip is not None:
+        zip_filename = os.path.basename(delete_deployment_zip)
+        s3_delete_key = f'hubapi_delete/{key_id}/{zip_filename}'
+        s3.put(filepath=delete_deployment_zip,
+               key=s3_delete_key)
+
+    if docker_cred_deployment_zip is not None:
+        zip_filename = os.path.basename(docker_cred_deployment_zip)
+        s3_docker_cred_key = f'docker_auth/{key_id}/{zip_filename}'
+        s3.put(filepath=docker_cred_deployment_zip,
+               key=s3_docker_cred_key)
+
+    cfn_yml = read_file_content(filepath=template)
+
+    hostname, username, password, database_name, collection_name, docker_username, docker_password = read_environment()
+    parameters = [
+        {'ParameterKey': 'DefS3Bucket', 'ParameterValue': S3_DEFAULT_BUCKET},
+        {'ParameterKey': 'HubListLambdaFnS3Key', 'ParameterValue': s3_list_key},
+        {'ParameterKey': 'HubPushLambdaFnS3Key', 'ParameterValue': s3_push_key},
+        {'ParameterKey': 'HubAPIAuthorizeLambdaFnS3Key', 'ParameterValue': s3_authorize_key},
+        {'ParameterKey': 'HubDeleteFnS3Key', 'ParameterValue': s3_delete_key},
+        {'ParameterKey': 'DockerCredFetcherLambdaFnS3Key', 'ParameterValue': s3_docker_cred_key},
+        {'ParameterKey': 'DefLambdaRole', 'ParameterValue': 'arn:aws:iam::416454113568:role/lambda-role'},
+        {'ParameterKey': 'DeploymentStage', 'ParameterValue': deployment_stage},
+        {'ParameterKey': 'JinaDBHostname', 'ParameterValue': hostname},
+        {'ParameterKey': 'JinaDBUsername', 'ParameterValue': username},
+        {'ParameterKey': 'JinaDBPassword', 'ParameterValue': password},
+        {'ParameterKey': 'JinaDBCollection', 'ParameterValue': collection_name},
+        {'ParameterKey': 'JinaDBName', 'ParameterValue': database_name},
+        {'ParameterKey': 'JinaDockerUsername', 'ParameterValue': docker_username},
+        {'ParameterKey': 'JinaDockerPassword', 'ParameterValue': docker_password}
+    ]
+
+    try:
+        with CFNStack(name=stack_name, template=cfn_yml,
+                      parameters=parameters, delete_at_exit=False) as api_cfn_stack:
+            if not api_cfn_stack.exists:
+                logger.error(f'Stack creation/update failed. Exiting context \n')
+                sys.exit(1)
+            logger.info('Resources description -- ')
+            for resource in api_cfn_stack.resources:
+                logger.info(f'Name: `{resource["LogicalResourceId"]}`\t\tType: `{resource["ResourceType"]}`\t'
+                            f'ID: `{resource["PhysicalResourceId"]}`')
+    except (StackCreationFailed, StackUpdateFailed) as cfn_exp:
+        logger.exception(f'Stack creation/update failed. Exiting context \n{cfn_exp}')
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    trigger()
---
+++ backup/delete-api/mongodb_endpoints/delete-api/hubapi_delete.py
@@ -0,0 +1,182 @@
+import os
+import json
+import base64
+import logging
+from typing import Optional, Dict, List, Union
+
+import pymongo
+
+
+def get_logger(context='generic', file=True):
+    logger = logging.getLogger(context)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+class MongoDBException(Exception):
+    """ Any errors raised by MongoDb """
+
+
+class MongoDBHandler:
+    """
+    Mongodb Handler to connect to the database & insert documents in the collection
+    """
+
+    def __init__(self, hostname: str, username: str, password: str,
+                 database_name: str, collection_name: str):
+        self.logger = get_logger(self.__class__.__name__)
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.database_name = database_name
+        self.collection_name = collection_name
+        self.connection_string = \
+            f'mongodb+srv://{self.username}:{self.password}@{self.hostname}'
+
+    def __enter__(self):
+        return self.connect()
+
+    def connect(self) -> 'MongoDBHandler':
+        try:
+            self.client = pymongo.MongoClient(self.connection_string)
+            self.client.admin.command('ismaster')
+            self.logger.info('Successfully connected to the database')
+        except pymongo.errors.ConnectionFailure:
+            raise MongoDBException('Database server is not available')
+        except pymongo.errors.ConfigurationError:
+            raise MongoDBException('Credentials passed are not correct!')
+        except pymongo.errors.PyMongoError as exp:
+            raise MongoDBException(exp)
+        except Exception as exp:
+            raise MongoDBException(exp)
+        return self
+
+    @property
+    def database(self):
+        return self.client[self.database_name]
+
+    @property
+    def collection(self):
+        return self.database[self.collection_name]
+
+    def delete(self, query: Dict[str, Union[Dict, List]]) -> None:
+        try:
+            # Deletes all the entries matching the query criteria from mongo.
+            # justOne is set to False to delete all matching entries.
+            return self.collection.remove(query, False)
+        except pymongo.errors.PyMongoError as exp:
+            self.logger.error(f'got an error while deleting a document in the db {exp}')
+
+    def insert(self, document: str) -> Optional[str]:
+        try:
+            result = self.collection.insert_one(document)
+            self.logger.info(f'Pushed current summary to the database')
+            return result.inserted_id
+        except pymongo.errors.PyMongoError as exp:
+            self.logger.error(f'got an error while inserting a document in the db {exp}')
+
+    def replace(self, document: Dict, query: Dict):
+        try:
+            result = self.collection.replace_one(query, document)
+            return result.modified_count
+        except pymongo.errors.PyMongoError as exp:
+            self.logger.error(f'got an error while replacing a document in the db {exp}')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.client.close()
+        except pymongo.errors.PyMongoError as exp:
+            raise MongoDBException(exp)
+
+
+def is_db_envs_set():
+    """ Checks if any of the db env variables are not set """
+    keys = ['JINA_DB_HOSTNAME', 'JINA_DB_USERNAME', 'JINA_DB_PASSWORD', 'JINA_DB_NAME', 'JINA_DB_COLLECTION']
+    return all(len(os.environ.get(k, '')) > 0 for k in keys)
+
+
+def _query_builder(params: Dict):
+    logger = get_logger(context='query_builder')
+    logger.info(f'Got the following parans: {params}')
+
+    if not params:
+        return {}, 0
+
+    sub_query = []
+    if 'kind' in params:
+        kind_query = {'manifest_info.kind': params['kind']}
+        sub_query.append(kind_query)
+    if 'type' in params:
+        type_query = {'manifest_info.type': params['type']}
+        sub_query.append(type_query)
+    if 'name' in params:
+        name_query = {'manifest_info.name': params['name']}
+        sub_query.append(name_query)
+    if 'keywords' in params:
+        keywords_list = params['keywords'].split(',')
+        keyword_query = {'manifest_info.keywords': {'$in': keywords_list}}
+        sub_query.append(keyword_query)
+
+    # A limit() value of 0 (i.e. limit(0)) is equivalent to setting no limit.
+    # Retrieves all matched documents.
+    limit = params.get('limit', 0)
+
+    if sub_query:
+        _executor_query = {'$and': sub_query}
+        logger.info(f'Query to search in mongodb: {_executor_query}')
+        return _executor_query, limit
+    else:
+        # select ALL
+        # force the cursor to iterate over each object
+        return {}, limit
+
+
+def _return_json_builder(body, status):
+    return {
+        "isBase64Encoded": False,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "statusCode": int(status),
+        "body": body
+    }
+
+
+def str_to_ascii_to_base64_to_str(text):
+    return base64.b64decode(text.encode('ascii')).decode('ascii')
+
+
+def read_environment():
+    hostname = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_HOSTNAME'))
+    username = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_USERNAME'))
+    password = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_PASSWORD'))
+    database_name = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_NAME'))
+    collection_name = str_to_ascii_to_base64_to_str(os.environ.get('JINA_DB_COLLECTION'))
+    return hostname, username, password, database_name, collection_name
+
+
+def lambda_handler(event, context):
+    """Lambda handler to read data from Mongodb Atlas (Used to perform `jina hub delete`)
+    """
+    logger = get_logger(context='hub_delete')
+
+    if not is_db_envs_set():
+        logger.warning('MongoDB environment vars are not set! book-keeping skipped.')
+        return _return_json_builder(body='Invalid Lambda environment',
+                                    status=500)
+
+    _executor_query = _query_builder(params=event.get('queryStringParameters', {}))
+
+    hostname, username, password, database_name, collection_name = read_environment()
+    with MongoDBHandler(hostname=hostname, username=username, password=password,
+                        database_name=database_name, collection_name=collection_name) as db:
+        write_result = db.delete(*_executor_query)
+        if write_result:
+            if write_result['nRemoved'] > 0:
+                return _return_json_builder(body=json.dumps({"delete_info": write_result}),
+                                            status=200)
+            return _return_json_builder(body="No docs found for deletion",
+                                        status=400)
+
+    return _return_json_builder(body='Invalid filters passed',
+                                status=400)
