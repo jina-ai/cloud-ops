875733ccab79c913da13dd58c8afe2c6661365af
Susana Guzman <zuxi.w13@gmail.com>
2021-01-19 11:41:17 +0100

    feat: add faiss indexer

--- stress-example/nlp_search/app.py
+++ stress-example/nlp_search/app.py
@@ -29,7 +29,7 @@ def config(indexer_query_type):
     os.environ.setdefault('JINA_PORT', str(45678))
 
     if indexer_query_type == 'faiss':
-        os.environ['JINA_USES'] = os.environ.get('JINA_USES_FAISS', 'docker://annoy_indexer_image:test')
+        os.environ['JINA_USES'] = os.environ.get('JINA_USES_FAISS', 'docker://faiss_indexer_image:test')
         os.environ['JINA_USES_INTERNAL'] = 'pods/faiss_indexer.yml'
     elif indexer_query_type == 'annoy':
         os.environ['JINA_USES'] = os.environ.get('JINA_USES_ANNOY', 'docker://annoy_indexer_image:test')
