d2a8dad228b1e99aad29fe3ec2bf497119fd16d1
Cristian <cristian.mitroi@jina.ai>
2021-02-03 14:13:02 +0100

    fix: env vars

--- stress-example/run_image.sh
+++ stress-example/run_image.sh
@@ -10,32 +10,32 @@ then
   echo "export SLEEP_TIME=..." && exit 1
 fi
 
-if [ -z "$1" ]
+if [ -z "$TIME_LOAD" ]
 then
-  echo "No time (seconds) for running supplied. Use e.g. 'bash run_image.sh 60 4'" && exit 1
+  echo "No time (seconds) for running supplied. 'export TIME_LOAD=...'" && exit 1
 fi
 
-if [ -z "$2" ]
+if [ -z "$CONCURRENCY" ]
 then
-  echo "No concurrency (int). Use e.g. 'bash run_image.sh 60 4'" && exit 1
+  echo "No concurrency (int). Use e.g. 'export CONCURRENCY=...'" && exit 1
 fi
 
 bash ./env_vars_config.sh && \
-$PYTHON_EXEC app.py --jinad index --dataset image && \
+$PYTHON_EXEC app.py --jinad index --dataset $DATASET && \
 sleep $SLEEP_TIME && \
-$PYTHON_EXEC client.py --dataset image -t index -n 100 -l $1 -c $2 && \
+$PYTHON_EXEC client.py --dataset $DATASET -t index -n 100 -l $TIME_LOAD -c $CONCURRENCY && \
 export workspace=`cat ws.txt` && \
 export flow=`cat flow.txt` && \
 sleep $SLEEP_TIME && \
 $PYTHON_EXEC app.py --jinad remove --flow-id $flow && \
 sleep $SLEEP_TIME && \
-$PYTHON_EXEC app.py --jinad query --dataset image --ws $workspace && \
+$PYTHON_EXEC app.py --jinad query --dataset $DATASET --ws $workspace && \
 sleep $SLEEP_TIME && \
-$PYTHON_EXEC client.py --dataset image -t query -n 10 -l $1 -c $2 && \
+$PYTHON_EXEC client.py --dataset $DATASET -t query -n 10 -l $TIME_LOAD -c $CONCURRENCY && \
 sleep $SLEEP_TIME && \
 export flow=`cat flow.txt` && \
 $PYTHON_EXEC app.py --jinad remove --flow-id $flow && \
 sleep $SLEEP_TIME && \
-$PYTHON_EXEC app.py --jinad query --dataset image --ws $workspace && \
+$PYTHON_EXEC app.py --jinad query --dataset $DATASET --ws $workspace && \
 sleep $SLEEP_TIME && \
-$PYTHON_EXEC client.py --dataset image -t query -n 10 -l $1 -c $2
+$PYTHON_EXEC client.py --dataset $DATASET -t query -n 10 -l $TIME_LOAD -c $CONCURRENCY
