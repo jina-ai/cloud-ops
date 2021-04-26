d16a61ef36980da53e6ee49169bc1ff1ce613df6
niuzs-alan <zhisheng.niu@jina.ai>
2021-04-26 14:46:13 +0800

    Fix pip pkgs (#110)
    * feat(deb-systemd.sh): update command to install pkgs to the new ebs disk
    
    * fix(deb-systemd.sh): mkdir /usr/local/jina/tmp for TMPDIR
    
    * fix(deb-systemd.sh): cancel var tmpdir's define

--- scripts/deb-systemd.sh
+++ scripts/deb-systemd.sh
@@ -19,7 +19,8 @@ sudo bash <<INIT
         
     chmod -R 777 /usr/local/jina
     # install python packages except jinad
-    python3.8 -m pip install --target /usr/local/jina $*
+    mkdir /usr/local/jina/tmp
+    TMPDIR=/usr/local/jina/tmp python3.8 -m pip install --upgrade --no-cache-dir --target /usr/local/jina $*
     # install jinad
     python3.8 -m pip install --upgrade --target /usr/local/jina --pre "jina[daemon]"
 INIT
