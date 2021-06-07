75e370c2d88874e78ac500ade4f62537d9e2f1ed
Alan Zhisheng Niu <zhisheng.niu@jina.ai>
2021-06-07 14:47:34 +0800

    feat(deb-systemd): add `--pre` option for jinad installation (#115)

--- scripts/deb-systemd.sh
+++ scripts/deb-systemd.sh
@@ -21,7 +21,7 @@ sudo bash <<INIT
     mkdir /usr/local/jina/tmp
     TMPDIR=/usr/local/jina/tmp python3.8 -m pip install --upgrade --no-cache-dir --target /usr/local/jina --progress-bar off $*
     # install jinad
-    python3.8 -m pip install --upgrade --target /usr/local/jina --progress-bar off "jina[daemon]"
+    python3.8 -m pip install --pre --upgrade --target /usr/local/jina --progress-bar off "jina[daemon]"
     chmod -R 777 /usr/local/jina
 INIT
 
